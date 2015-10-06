ac1f013fe0729361b3f014c4291cf8c78a1c4484
Stephen Mathews <busysteve@gmail.com>
2015-10-05 20:11:28 -0400

    This is the initial commit.

---
+++ famous_people_reader.cpp
@@ -0,0 +1,18 @@
+#include "rowparser.h"
+
+int main( int argc, char **argv )
+{
+
+	RowParser row( "./famous_people.csv", ",", "\n", false, true );
+
+	while( row.ParseNextRow() )
+	{
+		printf("Occupation %10.10s   |   Who : %s %s\n", 
+			row["fame"].c_str(), row["fname"].c_str(), row["lname"].c_str() );
+	}
+
+
+}
+
+
+
